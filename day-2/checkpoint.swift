fb051de57708105aeccfb9e5c4b32abc38d0e43d
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-16 16:50:45 -0800

    My solution to Hacking with Swift Checkpoint 1 - playground to convert celsius value to fahrenheit and print both

---
+++ day-2/checkpoint.swift
@@ -0,0 +1,13 @@
+// Your goal is to write a Swift playground that:
+
+// Creates a constant holding any temperature in Celsius.
+
+// Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+
+// Prints the result for the user, showing both the Celsius and Fahrenheit values.
+
+let celsius = 32.0
+let fahrenheit = ((celsius * 9) / 5 ) + 32
+
+print("\(celsius)°C")
+print("\(fahrenheit)°F")
