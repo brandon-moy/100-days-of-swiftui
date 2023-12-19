bc96f0764b0d318ec4de312e6cee58ecbcdcbc0a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-19 10:56:17 -0800

    Notes and examples for switch statements in Swift

---
+++ day-5/switch.swift
@@ -0,0 +1,80 @@
+// Instead of chaining else if statements, we can use a switch statement that will check each case and run code based on the case.
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack an umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == .snow {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+// In the case of an enum, Swift knows all possible cases the enum can have so it we miss one or check the same one twice, Swift will complain
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+// You can set a default response with the default: call at the end of the switch statement
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+// BUT make sure that default is at the end. If default is at the beginning, then it will ONLY return the default
+switch place {
+default:
+    print("Who are you?")
+    // These cases will not be reachable
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+}
+
+// Not commonly used but you can use the fallthrough call if you want Swift to carry on subsequent cases
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
