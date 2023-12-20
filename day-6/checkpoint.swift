45555c96c365b607e30d093ea19e2820d3cd9cc5
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-20 11:53:40 -0800

    Remade solution to FizzBuzz using isMultiple instead of % operator using hints after initial solution

--- day-6/checkpoint.swift
+++ day-6/checkpoint.swift
@@ -1,9 +1,23 @@
+// Initial solution to "FizzBuzz"
 for i in 1...100 {
   if i % 3 == 0 && i % 5 == 0 {
     print("FizzBuzz")
   } else if i % 3 == 0 {
     print("Fizz")
   } else if i % 5 == 0 {
+    print("Buzz")
+  } else {
+    print(i)
+  }
+}
+
+// Solution to "FizzBuzz" after hints - uses isMultiple instead of the modulo operator
+for i in 1...100 {
+  if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+    print("FizzBuzz")
+  } else if i.isMultiple(of: 3) {
+    print("Fizz")
+  } else if i.isMultiple(of: 5) {
     print("Buzz")
   } else {
     print(i)
