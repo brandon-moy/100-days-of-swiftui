dadd6fc26f7092d9e89f2f9e2e4b4957915b3b25
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-27 11:21:55 -0800

    Changed filter from /usr/bin/bash % 2 != 0 to using isMultiple after viewing hints in checkpoint

--- day-9/checkpoint.swift
+++ day-9/checkpoint.swift
@@ -17,7 +17,7 @@ let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 49 is a lucky number
 
 let luckyNumbersFiltered = luckyNumbers
-    .filter{$0 % 2 != 0}
+    .filter{!$0.isMultiple(of: 2)}
     .sorted{$0 < $1}
     .map{"\($0) is a lucky number" }
     
