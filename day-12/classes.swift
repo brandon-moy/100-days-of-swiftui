9ddf1eb6f02a21cf4a7665d9b9cc1794a9645ae1
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-01-03 19:28:39 -0800

    Notes and examples for initializers, deinitializers, and copying instances of a class in Swift

--- day-12/classes.swift
+++ day-12/classes.swift
@@ -82,3 +82,105 @@ class Vehicle {
         self.isElectric = isElectric
     }
 }
+// and let's say we want a Car class inheriting from Vehicle
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isConvertible: Bool) {
+        self.isConvertible = isConvertible
+    }
+}
+// BUT this has issues because the Vehicle class needs to know whether it's electric or not, in order to fix this we need to ask the super class to run the initializer
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+// now that we have valid initializers we can make an instance like so:
+let teslaX = Car(isElectric: true, isConvertible: false)
+
+// In Swift, all copies of a class instance share the same data, meaning that any changes you make to one copy will automatically change the other copies. 
+// This happens because classes are reference types in Swift, which means all copies of a class all refer back to the same underlying pot of data.
+
+// view this simple class for example:
+class User {
+    var username = "Anonymous"
+}
+
+var user1 = User()
+// now we could take a copy of user1 and change the username value
+var user2 = user1
+user2.username = "Taylor"
+// but this way, it prints Taylor for both
+print(user1.username)  
+print(user2.username)
+
+// if we were to change this to a struct from a class, we would print Anoymous and then Taylor because it won't adjust the original.
+
+// we can also create a unique copy of a class instance, a deep copy
+class User {
+    var username = "Anonymous"
+
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+
+// This way, we could call copy() to get an object with the same starting data, but any future changes won't impact the original
+
+
+// Swift’s classes can optionally be given a deinitializer, which is a bit like the opposite of an initializer 
+// in that it gets called when the object is destroyed rather than when it’s created.
+
+// this comes with a few small provisos:
+// Just like initializers, you don’t use func with deinitializers – they are special.
+// Deinitializers can never take parameters or return data, and as a result aren’t even written with parentheses.
+// Your deinitializer will automatically be called when the final copy of a class instance is destroyed. That might mean it was created inside a function that is now finishing, for example.
+// We never call deinitializers directly; they are handled automatically by the system.
+// Structs don’t have deinitializers, because you can’t copy them.
+
+// When deinitializers are called depends on scope - the context where information is available
+// If you create a variable inside a function, you can’t access it from outside the function.
+// If you create a variable inside an if condition, that variable is not available outside the condition.
+// If you create a variable inside a for loop, including the loop variable itself, you can’t use it outside the loop.
+
+// Here is a class that prints a message when it's created and destroyed
+class User {
+    let id: Int
+
+    init(id: Int) {
+        self.id = id
+        print("User \(id): I'm alive!")
+    }
+
+    deinit {
+        print("User \(id): I'm dead!")
+    }
+}
+// one example to create and destroy instances using a loop - if we create User instance inside the loop, it will be destroyed when the loop iteration finishes:
+for i in 1...3 {
+    let user = User(id: i)
+    print("User \(user.id): I'm in control!")
+}
+
+
+// Remember, the deinitializer is only called when the last remaining reference to a class instance is destroyed. 
+// This might be a variable or constant you have stashed away, or perhaps you stored something in an array.
+
+// For example, if we were adding our User instances as they were created, they would only be destroyed when the array is cleared:
+var users = [User]()
+
+for i in 1...3 {
+    let user = User(id: i)
+    print("User \(user.id): I'm in control!")
+    users.append(user)
+}
+
+print("Loop is finished!")
+users.removeAll()
+print("Array is clear!")
